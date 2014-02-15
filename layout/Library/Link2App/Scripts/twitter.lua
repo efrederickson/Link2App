40550593538c7dd1df97b42e7149f18674fee3f9
Elijah Frederickson <elijah.frederickson@gmail.com>
2014-02-15 11:42:49 -0900

    added support for twitter posts

--- layout/Library/Link2App/Scripts/twitter.lua
+++ layout/Library/Link2App/Scripts/twitter.lua
@@ -1,7 +1,10 @@
 return function(url)
     local username = url:gmatch("twitter.com/([%w_]+)")() -- extract username
     local _, ext = url:gmatch("twitter.com/([%w_]+)(/.+)")() -- check for other parts e.g. /post/1348whatever2
+    local statusid = url:gmatch("twitter.com/[%w_]+/statuses/(%d+)")()
     if username and not ext then
         return "twitter://user?screen_name=" .. username
+    elseif statusid then
+        return "twitter://status?id=" .. statusid
     end
 end
