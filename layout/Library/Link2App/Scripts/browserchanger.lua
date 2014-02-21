39109ecba76126f23726f6f2a3044d9e2bded815
Elijah Frederickson <elijah.frederickson@gmail.com>
2014-02-20 21:20:36 -0900

    Updated script filenames

--- layout/Library/Link2App/Scripts/browserchanger.lua
+++
@@ -1,8 +0,0 @@
-return function(url)
-    if url:sub(1, 5) == 'http:' then
-        return "googlechrome:" .. url:sub(6)
-    end
-    if url:sub(1, 6) == 'https:' then
-        return "googlechromes:" .. url:sub(7)
-    end
-end
