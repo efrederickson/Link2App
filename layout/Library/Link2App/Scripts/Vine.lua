2cfdef05ecd650794b833cb35c664be1db926a4b
Elijah Frederickson <elijah.frederickson@gmail.com>
2014-02-22 14:11:02 -0900

    Manually added Vine.lua script because of merge conflicts

---
+++ layout/Library/Link2App/Scripts/Vine.lua
@@ -0,0 +1,22 @@
+return function(url)
+     local numberedVineUser = url:gmatch("vine.co/u/(.+)")()
+     if numberedVineUser then
+         return "vine://user/" .. numberedVineUser
+     end
+ end
+
+ --[[
+ Other known URL types include:
+ vine://user-id/
+ vine://tw/user/
+ vine://post/
+ vine://tag/
+
+ -user, tw/user, user-id are all function the same from what i can tell?
+ -tag will search hashtags, though i dont believe the user will encounter a web URL like this
+ -post will link directly to a post, given the correct post "ID", which isn't in the URL
+ 	but rather under the meta tag "twitter:app:url:iphone"
+ -this will not work for users that have their own page, with a custom name, as its not numbered.
+ -there are some more URLs button they dont seem to do anything, so i wont bother listing them
+
+ ]]
