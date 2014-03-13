be57233ba9b0c4bd68ac915eb3cffbbd44da66f6
Elijah Frederickson <elijah.frederickson@gmail.com>
2014-03-13 11:44:44 -0800

    Update Browser Changer.lua

--- layout/Library/Link2App/Scripts/Browser Changer.lua
+++ layout/Library/Link2App/Scripts/Browser Changer.lua
@@ -1,3 +1,8 @@
+-- Browser changer (Safari -> Google Chrome)
+-- rpetrich has a list of "mappings" for different browsers.
+-- You can use that file to change this to redirect to your preferred browser
+--https://github.com/rpetrich/BrowserChooser/blob/master/layout/Library/Application%20Support/BrowserChooser/mapping.plist
+
 return function(url)
     if url:sub(1, 5) == 'http:' then
         return "googlechrome:" .. url:sub(6)
